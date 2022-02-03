5f59658d16327625362f2e68d285c88d9dc5e1cd
Victor Volkov <vityan2504@gmail.com>
2022-02-03 18:12:14 +0300

    add (script.js) did play bot

--- script.js
+++ script.js
@@ -1,45 +1,50 @@
 'use strict';
 
-const randomNum = Math.floor(Math.random() * (100 - 1)) + 1;
-
 const isNumber = function(num) {
    return !isNaN(parseFloat(num)) && isFinite(num);
 };
 
-const startGame = function(num, min, max) {
-   const playerNum = prompt(`Угадайте число от ${min} до ${max}`);
+const startGame = function(min, max) {
+   const randomNum = Math.floor(Math.random() * (100 - 1 + 1)) + 1;
 
-   const guessTheNumber = function() {
+   const playing = function(num) {
+      const playerNum = prompt(`Угадайте число от ${min} до ${max}`);
       
-      if (playerNum === null) {
-         alert('Игра окончена');
-         return;
-      }
-      if (!isNumber(playerNum) || playerNum === '') {
-         alert('Введите число!');
-         startGame(num, min, max);
-      } else {
-            if (!(playerNum > 0 && playerNum <= 100)) { 
-               alert(`Число ${playerNum} выходит из диапозона чисел от ${min} до ${max}`);
-               startGame(num, min, max);
-            } else {
-            if  (playerNum < num) {
-               alert('Загаданное число больше');
-               startGame(num, min, max);
-            }
-            if (playerNum > num) {
-               alert('Загаданное число меньше');
-               startGame(num, min, max);
-            }
-            if (+playerNum === num) {
-               alert('Поздравляю, Вы угадали!!!');
-               return;
+      const guessTheNumber = function() {
+      
+         if (playerNum === null) {
+            alert('Игра окончена');
+            return;
+         }
+         if (!isNumber(playerNum) || playerNum === '') {
+            alert('Введите число!');
+            playing(randomNum);
+         } else {
+               if (!(playerNum > 0 && playerNum <= 100)) { 
+                  alert(`Число ${playerNum} выходит из диапозона чисел от ${min} до ${max}`);
+                  playing(randomNum);
+               } else {
+               if  (playerNum < num) {
+                  alert('Загаданное число больше');
+                  playing(randomNum);
+               }
+               if (playerNum > num) {
+                  alert('Загаданное число меньше');
+                  playing(randomNum);
+               }
+               if (+playerNum === num) {
+                  alert('Поздравляю, Вы угадали!!!');
+                  return;
+               }
             }
          }
-      }
+      };
+      
+      guessTheNumber();
    };
 
-   guessTheNumber();
+   playing(randomNum);
 };
 
-const game = startGame(randomNum, 1, 100);
+
+const game = startGame(1, 100);
